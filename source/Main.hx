edce07de398ed7b2ca4490db306e0d5fc08de4fa
SantiYea <95106797+SantiYea@users.noreply.github.com>
2022-09-18 16:53:30 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,19 +1,18 @@
 package;
 
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
+import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
 import openfl.Lib;
-import openfl.display.FPS;
+import FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-#if android //only android will use those
-import sys.FileSystem;
-import lime.app.Application;
-import lime.system.System;
-import android.*;
-#end
+import openfl.display.StageScaleMode;
+
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
@@ -24,7 +23,8 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
-	public static var path:String = System.applicationStorageDirectory;																
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
@@ -33,9 +33,8 @@ class Main extends Sprite
 
 	public function new()
 	{
-
+		SUtil.uncaughtErrorHandler();
 		super();
-		SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -47,8 +46,6 @@ class Main extends Sprite
 		}
 	}
 
-          
-
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
@@ -57,6 +54,10 @@ class Main extends Sprite
 		}
 
 		setupGame();
+		var timer = new haxe.Timer(1);
+		timer.run = function() {
+		coloring();
+		if (fpsVar.textColor == 0) fpsVar.textColor = -4775566;} // needs to be done because textcolor becomes black for a frame
 	}
 
 	private function setupGame():Void
@@ -75,14 +76,19 @@ class Main extends Sprite
 
 		#if !debug
 		initialState = TitleState;
-		#end     
-
+		#end
+	
+		SUtil.check();
 		ClientPrefs.loadDefaultKeys();
-		SUtil.doTheCheck();
+		// fuck you, persistent caching stays ON during sex
+		FlxGraphic.defaultPersist = true;
+		// the reason for this is we're going to be handling our own cache smartly
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
-
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
@@ -92,5 +98,43 @@ class Main extends Sprite
 		FlxG.mouse.visible = false;
 		#end
 	}
-}
 
+	// Chroma Effect (12 Colors)
+	var array:Array<FlxColor> = [
+		FlxColor.fromRGB(216, 34, 83),
+		FlxColor.fromRGB(255, 38, 0),
+		FlxColor.fromRGB(255, 80, 0),
+		FlxColor.fromRGB(255, 147, 0),
+		FlxColor.fromRGB(255, 199, 0),
+		FlxColor.fromRGB(255, 255, 0),
+		FlxColor.fromRGB(202, 255, 0),
+		FlxColor.fromRGB(0, 255, 0),
+		FlxColor.fromRGB(0, 146, 146),
+		FlxColor.fromRGB(0, 0, 255),
+		FlxColor.fromRGB(82, 40, 204),
+		FlxColor.fromRGB(150, 33, 146)
+	];
+	var skippedFrames = 0;
+	var currentColor = 0;
+
+	// Event Handlers
+	public function coloring():Void
+	{
+		// Hippity, Hoppity, your code is now my property (from KadeEngine and from a fork)
+		if (ClientPrefs.rainbowFps) {
+		if (currentColor >= array.length)
+			currentColor = 0;
+		currentColor = Math.round(FlxMath.lerp(0, array.length, skippedFrames / ClientPrefs.framerate));
+		(cast(Lib.current.getChildAt(0), Main)).changeFPSColor(array[currentColor]);
+		currentColor++;
+		skippedFrames++;
+		if (skippedFrames > ClientPrefs.framerate)
+			skippedFrames = 0;
+		}
+		else fpsVar.textColor = FlxColor.fromRGB(255, 255, 255);
+	}
+	public function changeFPSColor(color:FlxColor)
+	{
+		fpsVar.textColor = color;
+	}
+}
